0a6c127684124fa9bfbf298fcd4ff66371b9aa16
MVSGMeghana <93912963+MVSGMeghana@users.noreply.github.com>
2021-11-25 09:46:19 +0530

    loops

---
+++ loops 180.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main()
+{
+int a,b;
+printf("enter values of a ");
+scanf("%d",&a);
+printf("enter values of b ");
+scanf("%d",&b);
+if(a>b)
+{
+printf("%d",a);
+}
+else
+{
+printf("%d",b);
+}
+return 0;
+}
